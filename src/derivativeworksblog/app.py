e71c4bcc73d94fd1784455be119624aa8bb3d805
Michael Mason <TeaUponTweed@gmail.com>
2022-09-25 14:45:08 -0600

    move everything

---
+++ src/derivativeworksblog/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, send_file, send_from_directory
+
+# set the project root directory as the static folder, you can set others.
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return send_file("static/index.html")
+
+
+@app.route("/static/<path:path>")
+def send_static(path):
+    return send_from_directory("static", path)
+
+
+@app.route("/feed")
+def feed():
+    return send_file("static/rss.xml")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
--- src/derivativeworksblog/complie_all.py
+++ src/derivativeworksblog/complie_all.py
@@ -1,13 +1,14 @@
-import os
 import glob
+import os
+import sys
 
 import markdown2
 
 
-def main():
-    posts = sorted(glob.glob('posts/*.md'),reverse=True)
+def main(postdir, outdir):
+    posts = sorted(glob.glob(os.path.join(postdir, "*.md")), reverse=True)
     compiled_posts = {}
-    header_html = '''
+    header_html = """
     <header>
       <h1>
         <a href="#home">M. B. Mason's Blog</a>
@@ -18,13 +19,13 @@ def main():
         <a href="/static/rss.xml" type="application/rss+xml">Feed</a>
       </nav>
     </header>
-'''
-    for i,post in enumerate(posts):
-        i = len(posts)-i
+"""
+    for i, post in enumerate(posts):
+        i = len(posts) - i
         post_html = markdown2.markdown_path(post)
         date = os.path.basename(post)[:8]
-        title = os.path.basename(post)[8:-3].replace('_',' ')
-        doc_html = '''
+        title = os.path.basename(post)[8:-3].replace("_", " ")
+        doc_html = """
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -48,12 +49,14 @@ def main():
     </main>
     </body>
 </html>
-'''.format(title=title, header=header_html, body=post_html)
-        compiled_post = f'static/post{i}.html'
-        print(doc_html, file=open(compiled_post,'w'))
-        compiled_posts[title,date] = compiled_post
+""".format(
+            title=title, header=header_html, body=post_html
+        )
+        compiled_post = os.path.join(outdir,f"post{i}.html")
+        print(doc_html, file=open(compiled_post, "w"))
+        compiled_posts[title, date] = compiled_post
 
-    index_html = '''
+    index_html = """
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -96,14 +99,16 @@ def main():
     </section>
   </body>
 </html>
-'''.format(
-    header=header_html,
-    body='\n'.join(f'<li><a href="/{post}"> {title} </a> </li>' for (title,date),post in compiled_posts.items())
-)
-    print(index_html, file=open('static/index.html','w'))
+""".format(
+        header=header_html,
+        body="\n".join(
+            f'<li><a href="/{post}"> {title} </a> </li>'
+            for (title, date), post in compiled_posts.items()
+        ),
+    )
+    print(index_html, file=open(os.path.join(outdir, "index.html"), "w"))
 
-
-    rss_html = '''
+    rss_html = """
 <rss version="2.0">
     <channel>
         <title>M. B. Mason's Blog</title> 
@@ -113,8 +118,13 @@ def main():
 {}
     </channel>
 </rss>
-'''.format('\n'.join(f'        <item> <title>{title}</title> <link>https://blog.derivativeworks.co/{post}</link> <pubDate>{format_date_for_rss(date)}</pubDate> </item>' for (title,date),post in compiled_posts.items()))
-    print(rss_html, file=open('static/rss.xml','w'))
+""".format(
+        "\n".join(
+            f"        <item> <title>{title}</title> <link>https://blog.derivativeworks.co/{post}</link> <pubDate>{format_date_for_rss(date)}</pubDate> </item>"
+            for (title, date), post in compiled_posts.items()
+        )
+    )
+    print(rss_html, file=open(os.path.join(outdir, "rss.xml"), "w"))
 
 
 def format_date_for_rss(date):
@@ -122,21 +132,21 @@ def format_date_for_rss(date):
     MM = date[4:6]
     DD = date[6:8]
     months = {
-        '01': "Jan",
-        '02': "Feb",
-        '03': "Mar",
-        '04': "Apr",
-        '05': "May",
-        '06': "Jun",
-        '07': "Jul",
-        '08': "Aug",
-        '09': "Sep",
-        '10': "Oct",
-        '11': "Nov",
-        '12': "Dec",
+        "01": "Jan",
+        "02": "Feb",
+        "03": "Mar",
+        "04": "Apr",
+        "05": "May",
+        "06": "Jun",
+        "07": "Jul",
+        "08": "Aug",
+        "09": "Sep",
+        "10": "Oct",
+        "11": "Nov",
+        "12": "Dec",
     }
-    return f'{DD} {months[MM]} {YYYY} 12:00:00 MST'
+    return f"{DD} {months[MM]} {YYYY} 12:00:00 MST"
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    main(*sys.argv[1:])
